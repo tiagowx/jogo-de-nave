0c210838a3180a65213faeab97d8b0b6296cccc1
wxtiago <tiagobatista.tec@gmail.com>
2021-12-06 12:46:34 -0300

    Parte 3

---
+++ js/js.js
@@ -0,0 +1,9 @@
+function start() { // Inicio da função start()
+
+    $("#inicio").hide();
+
+    $("#fundoGame").append("<div id='jogador'></div>");
+    $("#fundoGame").append("<div id='inimigo1'></div>");
+    $("#fundoGame").append("<div id='inimigo2'></div>");
+    $("#fundoGame").append("<div id='amigo'></div>");
+}
