499be10d5e2fe5a60cfc2d3f0beb14cb048692ca
wxtiago <tiagobatista.tec@gmail.com>
2021-12-06 13:30:41 -0300

    Parte 8

--- js/js.js
+++ js/js.js
@@ -10,6 +10,9 @@ function start() { // Inicio da função start()
 
     var jogo = {}
 
+    var velocidade = 5;
+    var posicaoY = parseInt(Math.random() * 334);
+
     var TECLA = {
         W: 87,
         S: 83,
@@ -39,6 +42,8 @@ function start() { // Inicio da função start()
 
         movefundo();
         movejogador();
+        moveinimigo1();
+
     } // Fim da função loop()
 
 
@@ -82,5 +87,18 @@ function start() { // Inicio da função start()
         }
 
     } // fim da função movejogador()
+    
+    function moveinimigo1() {
 
+        posicaoX = parseInt($("#inimigo1").css("left"));
+        $("#inimigo1").css("left", posicaoX - velocidade);
+        $("#inimigo1").css("top", posicaoY);
+
+        if (posicaoX <= 0) {
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left", 694);
+            $("#inimigo1").css("top", posicaoY);
+
+        }
+    } //Fim da função moveinimigo1()
 }
