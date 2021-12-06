eeff87e73f7ec6fcc11009580f0b7534c11b141e
wxtiago <tiagobatista.tec@gmail.com>
2021-12-06 13:34:27 -0300

    Parte 9

--- js/js.js
+++ js/js.js
@@ -43,6 +43,8 @@ function start() { // Inicio da função start()
         movefundo();
         movejogador();
         moveinimigo1();
+        moveinimigo2();
+        moveamigo();
 
     } // Fim da função loop()
 
@@ -87,7 +89,7 @@ function start() { // Inicio da função start()
         }
 
     } // fim da função movejogador()
-    
+
     function moveinimigo1() {
 
         posicaoX = parseInt($("#inimigo1").css("left"));
@@ -101,4 +103,30 @@ function start() { // Inicio da função start()
 
         }
     } //Fim da função moveinimigo1()
+
+    function moveinimigo2() {
+        posicaoX = parseInt($("#inimigo2").css("left"));
+        $("#inimigo2").css("left", posicaoX - 3);
+
+        if (posicaoX <= 0) {
+
+            $("#inimigo2").css("left", 775);
+
+        }
+    } // Fim da função moveinimigo2()
+
+
+    function moveamigo() {
+
+        posicaoX = parseInt($("#amigo").css("left"));
+        $("#amigo").css("left", posicaoX + 1);
+
+        if (posicaoX > 906) {
+
+            $("#amigo").css("left", 0);
+
+        }
+
+    } // fim da função moveamigo()
+
 }
