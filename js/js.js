d73828f9b1825d99caca690fbac319e248a568c4
wxtiago <tiagobatista.tec@gmail.com>
2022-01-10 23:50:10 -0300

    Parte 15

--- js/js.js
+++ js/js.js
@@ -7,6 +7,7 @@ function start() { // Inicio da função start()
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
     $("#fundoGame").append("<div id='placar'></div>");
+    $("#fundoGame").append("<div id='energia'></div>");
 
 
     //Principais variáveis do jogo
@@ -16,6 +17,8 @@ function start() { // Inicio da função start()
     var pontos = 0;
     var salvos = 0;
     var perdidos = 0;
+
+    var energiaAtual = 3;
 
     var fimdejogo = false;
     var podeAtirar = true;
@@ -58,6 +61,7 @@ function start() { // Inicio da função start()
         moveamigo();
         colisao();
         placar();
+        energia();
 
     } // Fim da função loop()
 
@@ -187,6 +191,8 @@ function start() { // Inicio da função start()
 
         // jogador com o inimigo1
         if (colisao1.length > 0) {
+            energiaAtual--;
+
             inimigo1X = parseInt($("#inimigo1").css("left"));
             inimigo1Y = parseInt($("#inimigo1").css("top"));
 
@@ -200,6 +206,8 @@ function start() { // Inicio da função start()
 
         // jogador com o inimigo2 
         if (colisao2.length > 0) {
+            energiaAtual--;
+
             inimigo2X = parseInt($("#inimigo2").css("left"));
             inimigo2Y = parseInt($("#inimigo2").css("top"));
 
@@ -373,5 +381,33 @@ function start() { // Inicio da função start()
         $("#placar").html("<h2> Pontos: " + pontos + " Salvos: " + salvos + " Perdidos: " + perdidos + "</h2>");
 
     } //fim da função placar()
+
+    //Barra de energia
+
+    function energia() {
+
+        if (energiaAtual == 3) {
+
+            $("#energia").css("background-image", "url(imgs/energia3.png)");
+        }
+
+        if (energiaAtual == 2) {
+
+            $("#energia").css("background-image", "url(imgs/energia2.png)");
+        }
+
+        if (energiaAtual == 1) {
+
+            $("#energia").css("background-image", "url(imgs/energia1.png)");
+        }
+
+        if (energiaAtual == 0) {
+
+            $("#energia").css("background-image", "url(imgs/energia0.png)");
+
+            //Game Over
+        }
+
+    } // Fim da função energia()
 
 }
